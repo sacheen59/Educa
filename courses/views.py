eb26c766bae8b958ad98d3bbed9433cef796773c
sachin barali <baralisachin5925@gmail.com>
2024-11-20 22:23:15 +0545

    worked on course module

--- courses/views.py
+++ courses/views.py
@@ -1,8 +1,13 @@
 from django.urls import reverse_lazy
+from django.apps import apps
+from django.forms.models import modelform_factory
 from django.views.generic.edit import CreateView,DeleteView,UpdateView
 from django.views.generic import ListView
-from courses.models import Course
+from courses.models import Course,Module,Content
 from django.contrib.auth.mixins import (LoginRequiredMixin,PermissionRequiredMixin)
+from django.shortcuts import get_object_or_404,redirect
+from django.views.generic.base import TemplateResponseMixin,View
+from .forms import ModuleFormSet
 
 # Create your views here.
 class OwnerMixin:
@@ -42,3 +47,103 @@ class CourseUpdateView(OwnerCourseEditMixin,UpdateView):
 class CourseDeleteView(OwnerCourseEditMixin,DeleteView):
     template_name = 'courses/manage/course/delete.html'
     permission_required = "courses.delete_course"
+
+class CourseModuleUpdateView(TemplateResponseMixin,View):
+    template_name = "courses/manage/module/formset.html"
+    course = None
+
+    def get_formset(self,data=None):
+        return ModuleFormSet(data=data,instance=self.course)
+    
+    def dispatch(self,request,pk):
+        self.course = get_object_or_404(Course,id=pk,owner=request.user)
+        return super().dispatch(request,pk)
+    
+    def get(self,request,*args,**kwargs):
+        formset = self.get_formset()
+        return self.render_to_response({
+            'course':self.course,
+            'formset': formset
+        })
+    
+    def post(self,request,*args,**kwargs):
+        formset = self.get_formset(data= request.POST)
+        if formset.is_valid():
+            formset.save()
+            return redirect('manage_course_list')
+        return self.render_to_response(
+            {
+                'course':self.course,
+                'formset': formset
+            }
+        )
+    
+class ContentCreateUpdateView(TemplateResponseMixin,View):
+    module = None
+    model = None
+    obj = None
+    template_name = 'course/manage/content/form.html'
+
+    def get_model(self,model_name):
+        if model_name in ['text','video','image','file']:
+            return apps.get_model(
+                app_label='courses',
+                model_name=model_name
+            )
+        return None
+    
+    def get_form(self,model,*args,**kwargs):
+        Form = modelform_factory(
+            model,
+            exclude=['owner','order','created','updated']
+        )
+        return Form(*args,**kwargs)
+    
+    def dispatch(self,request,module_id,model_name,id = None):
+        self.module = get_object_or_404(
+            Module,
+            id = module_id,
+            course__owner = request.user
+        )
+        self.model = self.get_model(model_name)
+        if id:
+            self.obj = get_object_or_404(
+                self.model,
+                id = id,
+                owner = request.user
+            )
+        return super().dispatch(request,module_id,model_name,id)
+
+    def get(self,request,module_id,model_name,id=None):
+        form = self.get_form(self.model,instance = self.obj)
+        return self.render_to_response(
+            {
+                'form':form,
+                'object':self.obj
+            }
+        )
+    
+    def post(self,request,module_id, model_name,id=None):
+        form = self.get_form(
+            self.model,
+            instance= self.obj,
+            data = request.POST,
+            files = request.FILES
+        )
+
+        if form.is_vlaid():
+            obj = form.save(commit=False)
+            obj.owner = request.user
+            obj.save()
+
+            if not id:
+                Content.objects.create(module = self.module, item = self.obj)
+            return redirect('module_content_list',self.module,id)
+        
+        return self.render_to_response(
+            {
+                'form':form,
+                'object':self.obj
+            }
+        )
+    
